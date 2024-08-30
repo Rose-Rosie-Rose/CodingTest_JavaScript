d05ea56f9689b943fcfad4c77b952e717ff0be16
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-08-30 15:23:32 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 26.73 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,9 @@
+const solution = (arr) => {
+  return arr.map((item) =>
+    item >= 50 && item % 2 === 0
+      ? item / 2
+      : item < 50 && item % 2 !== 0
+      ? item * 2
+      : item
+  );
+};
