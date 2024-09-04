a6451b5df8da24fc0dfc1165da029c27fb2cf5d4
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-09-04 10:46:35 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+const solution = (n) => {
+  let arr = [];
+
+  while (n !== 1) {
+    arr.push(n);
+
+    if (n % 2 === 0) {
+      n = n / 2;
+    } else {
+      n = 3 * n + 1;
+    }
+  }
+
+  arr.push(1);
+  return arr;
+};
