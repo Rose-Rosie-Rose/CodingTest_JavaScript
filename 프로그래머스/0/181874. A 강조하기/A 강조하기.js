fb3bcb21065c57a2e5ee6ffd2fe99642b5be23a9
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-09-05 18:01:31 +0900

    [level 0] Title: A 강조하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,6 @@
+const solution = (myString) => {
+    return myString
+        .split("")
+        .map(char => char === 'a' ? 'A' : (char === 'A' ? 'A' : char.toLowerCase()))
+        .join("");
+}
