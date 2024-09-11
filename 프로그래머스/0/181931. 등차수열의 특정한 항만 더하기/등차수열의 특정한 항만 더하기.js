26795307de620eb5a0d7f8ff46759b83c63c1ebc
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-09-11 17:44:40 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,11 @@
+const solution = (a, d, included) => {
+    let sum = 0;
+    
+    included.forEach((include, index) => {
+        if (include) {
+            sum += a + (d * index); 
+        }
+    });
+    
+    return sum;
+}
