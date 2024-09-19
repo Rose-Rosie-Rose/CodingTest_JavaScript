5e12919a5993c89437c941225b6024443ab4b316
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-09-19 10:17:53 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -0,0 +1,13 @@
+const solution = (arr, flag) => {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (flag[i]) {
+      result.push(...Array(arr[i] * 2).fill(arr[i]));
+    } else {
+      result.splice(-arr[i]);
+    }
+  }
+
+  return result;
+};
