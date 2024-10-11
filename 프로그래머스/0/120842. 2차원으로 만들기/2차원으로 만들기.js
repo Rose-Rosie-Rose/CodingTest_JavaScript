3478287cecd7b3c32c01233ca80849accfb1968c
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-10-11 12:37:17 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,8 +1,9 @@
 const solution = (num_list, n) => {
-    let result = []
+    const result = []
     
-    while(num_list.length) {
-        result.push(num_list.splice(0, n))
+    for(let i = 0; i < num_list.length; i += n){
+        result.push(num_list.slice(i, i + n))
     }
+    
     return result
 }
