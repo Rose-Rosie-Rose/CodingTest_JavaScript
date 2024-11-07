fba731f3034ff6ce307054c6908c18231478d605
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-11-07 16:55:04 +0900

    [level 0] Title: 1로 만들기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,16 @@
+const solution = (num_list) => {
+    let sum = 0;
+    
+    num_list.forEach(num => {
+        while (num > 1) {
+            if (num % 2 === 0) {
+                num /= 2;
+            } else {
+                num = (num - 1) / 2;
+            }
+            sum += 1;
+        }
+    });
+    
+    return sum;
+};
