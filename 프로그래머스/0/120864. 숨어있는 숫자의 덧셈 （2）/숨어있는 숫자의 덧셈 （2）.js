942b6b7607142f8bb8ae92fc9180e131a8d5545a
Rosie <88028208+Rose-Rosie-Rose@users.noreply.github.com>
2024-12-16 15:09:56 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -1,8 +1,5 @@
 const solution = (my_string) => {
-    const arr = my_string.match(/\d+/g) || [];
-    let sum = 0
-    for(let i = 0; i < arr.length; i++){
-        sum += parseInt(arr[i])
-    }
-    return sum
-}
+    return (my_string.match(/\d+/g) || [])
+        .map(Number)
+        .reduce((sum, num) => sum + num, 0);
+};
